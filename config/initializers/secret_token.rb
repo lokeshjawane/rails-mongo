b066a5270bd4f25b79f62830e5e62379f73975da
lokeshjawane <lucky.cs025@gmail.com>
2018-12-19 11:27:48 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailMongo::Application.config.secret_key_base = '4cad6a92712befd790e215df401afe9beb074f5bd0cca74628ecc77ec924ddf9b95d6ef94dbe7e96bd0d4c18ab8cb3a85b741f47f78f80a95a2dcde8b1c63f3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailMongo::Application.config.session_store :cookie_store, key: '_rail-mongo_session'
