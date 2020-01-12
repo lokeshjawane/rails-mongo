1d20186580019dea29b04963eaed12a31824ea57
Ubuntu <ubuntu@ip-172-31-93-129.ec2.internal>
2020-01-12 17:29:26 +0000

    initial comming on develop branch

---
+++ script.sh
@@ -0,0 +1,9 @@
+ex=$( cd "$(dirname "$0")" ; pwd -P )
+cd  $ex
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+echo $PATH
+gem install bundler -v  '1.17.3'
+bundle install
+bundle exec rake db:migrate
+bundle exec sidekiq
