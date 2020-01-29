92e74f06bb816cc4adcc48f0678889def0cec3f7
lokeshjawane <lokeshjawane@gmail.com>
2020-01-29 23:24:01 +0530

    changes in circle ci file

--- rake_test.sh
+++ rake_test.sh
@@ -2,5 +2,5 @@
 cd /mnt
 gem install bundler -v  '1.17.3'
 bundle install --path vendor/bundle
-bundle exec rake db:drop db:create db:migrate
+bundle exec rake db:drop db:create db:migrate --trace
 bundle exec rake test
