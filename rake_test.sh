103bf107df550cf371d84c377ab2b46fcbe5ebc6
lokeshjawane <lokeshjawane@gmail.com>
2020-01-30 10:17:14 +0530

    updated circle ci for s3 upload

--- rake_test.sh
+++ rake_test.sh
@@ -4,3 +4,4 @@ gem install bundler -v  '1.17.3'
 bundle install --path vendor/bundle
 bundle exec rake db:drop db:create db:migrate --trace
 bundle exec rake test
+zip -r /tmp/app-test.zip . -x "*.git*"
